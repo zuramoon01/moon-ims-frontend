8a1cbc5d8a710799d7046ce9ff8946231b7aa699
Zura Moon <zuramoon01@gmail.com>
2024-03-04 14:09:45 +0700

    update(ui): updated checkbox

--- src/lib/ui/checkbox/checkbox.svelte
+++ src/lib/ui/checkbox/checkbox.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import { Icon } from "$lib/ui";
+	import clsx from "clsx";
 	import { createCheckbox, melt } from "@melt-ui/svelte";
+	import { Icon } from "$lib/ui";
 
 	const {
 		elements: { root, input },
@@ -10,12 +11,17 @@
 
 <button
 	use:melt={$root}
-	class="flex size-4 items-center justify-center rounded-sm border border-havelock-blue-500"
+	class={clsx(
+		"flex size-4 items-center justify-center rounded border",
+		$isIndeterminate || $isChecked
+			? "border-accent-950 text-accent-950"
+			: "border-accent-950/20 text-accent-950/20",
+	)}
 >
 	{#if $isIndeterminate}
-		<Icon props={{ name: "remove", fill: "#4893db" }} />
+		<Icon props={{ name: "remove" }} />
 	{:else if $isChecked}
-		<Icon props={{ name: "check", fill: "#4893db" }} />
+		<Icon props={{ name: "check" }} />
 	{/if}
 
 	<input use:melt={$input} />
