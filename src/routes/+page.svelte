801ab0b0715dbfbf9efe7c0475a515f355a1b261
Zura Moon <zuramoon01@gmail.com>
2024-03-04 14:11:48 +0700

    updated(page): updated product ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
-	import { Checkbox } from "$lib/ui";
-	import { Icon } from "$lib/ui";
+	import clsx from "clsx";
+	import { Button, Checkbox, Icon } from "$lib/ui";
 	import { formatToRupiah } from "$lib/utils";
 
 	const tableTitles = [
-		"Nama Produk",
-		"Kuantitas",
-		"Harga Beli Per Satuan",
-		"Harga Jual Per Satuan",
-		"Total Harga Beli",
-		"Total Harga Jual",
+		{
+			text: "Nama Produk",
+			classes:
+				" min-w-[200px] w-[calc(100%_-_(32px_+_160px_+_240px_+_240px_+_200px_+_200px_+_80px))]",
+		},
+		{
+			text: "Kuantitas",
+			classes: "w-[160px] shrink-0",
+		},
+		{
+			text: "Harga Beli Per Satuan",
+			classes: "w-[240px] shrink-0",
+		},
+		{
+			text: "Harga Jual Per Satuan",
+			classes: "w-[240px] shrink-0",
+		},
+		{
+			text: "Total Harga Beli",
+			classes: "w-[200px] shrink-0",
+		},
+		{
+			text: "Total Harga Jual",
+			classes: "w-[200px] shrink-0",
+		},
 	];
 
 	const products = [
@@ -40,126 +59,153 @@
 	];
 </script>
 
-<main class="flex h-full w-full flex-col items-start">
-	<section
-		class="flex h-full w-full flex-col items-start rounded-md border border-havelock-blue-50 bg-white px-8 py-8"
-	>
-		<div class="flex w-full items-center pb-4">
-			<h1 class="text-[1.25rem] font-bold leading-none">Produk</h1>
-		</div>
+<main class="flex w-[calc(100%_-_theme(spacing.sidebar))] flex-col items-start">
+	<section class="flex w-full flex-col items-start px-8">
+		<header
+			class="flex h-[64px] w-full items-center justify-between px-4 text-accent-950"
+		>
+			<h1 class="text-xl font-bold leading-none">Produk</h1>
 
-		<div class="flex h-full w-full flex-col items-start overflow-auto">
+			<Button
+				props={{
+					text: "Tambah Produk",
+					icon: "plus",
+				}}
+			/>
+		</header>
+
+		<div
+			class="flex w-full flex-col items-start overflow-auto text-sm leading-none"
+		>
 			<div
-				class="grid w-full grid-cols-[32px_minmax(200px,_1fr)_200px_240px_240px_200px_200px_120px] border-b-2 border-havelock-blue-100"
+				class="flex h-8 w-full px-4 font-semibold text-accent-950 shadow-horizontal"
 			>
-				<div class="flex w-full items-center justify-center">
+				<div
+					class="flex w-8 shrink-0 items-center justify-center shadow-vertical"
+				>
 					<Checkbox />
 				</div>
 
-				{#each tableTitles as title, i (i)}
-					<div class="flex w-full items-center justify-between px-4 py-2">
-						<span class="w-full text-base font-semibold leading-none"
-							>{title}</span
-						>
+				{#each tableTitles as { text, classes }, i (i)}
+					<div
+						class={clsx(
+							"flex items-center justify-between px-4 shadow-vertical",
+							classes,
+						)}
+					>
+						<span>{text}</span>
 
-						<Icon props={{ name: "arrowDropDown" }} />
+						<Icon props={{ name: "arrowDropDown", classes: "size-5" }} />
 					</div>
 				{/each}
 
-				<div class="flex w-full items-center justify-between px-4 py-2">
-					<span class="w-full text-center text-base font-semibold leading-none"
-						>Aksi</span
-					>
+				<div class="flex w-20 shrink-0 items-center justify-center px-4">
+					<span class="text-center">Aksi</span>
 				</div>
 			</div>
 
 			{#each products as { name, quantity, buyPrice, sellPrice }, i (i)}
-				<div
-					class="grid w-full grid-cols-[32px_minmax(200px,_1fr)_200px_240px_240px_200px_200px_120px] border-b border-havelock-blue-100"
-				>
-					<div class="flex w-full items-center justify-center">
+				<div class="flex min-h-8 w-full px-4 shadow-horizontal">
+					<div
+						class="flex w-8 shrink-0 items-center justify-center shadow-vertical"
+					>
 						<Checkbox />
 					</div>
 
-					<div class="flex w-full items-center justify-between px-4 py-2">
-						<span class="w-full text-base leading-none">{name}</span>
+					<div
+						class="flex w-[calc(100%_-_(32px_+_160px_+_240px_+_240px_+_200px_+_200px_+_80px))] min-w-[200px] items-center px-4 py-1 shadow-vertical"
+					>
+						<span class="text-sm">{name}</span>
 					</div>
 
-					<div class="flex w-full items-center justify-between px-4 py-2">
-						<span class="w-full text-base leading-none">{quantity}</span>
+					<div
+						class="flex w-[160px] shrink-0 items-center px-4 py-1 shadow-vertical"
+					>
+						<span>{quantity}</span>
 					</div>
 
-					<div class="flex w-full items-center justify-between px-4 py-2">
-						<span class="w-full text-base leading-none"
-							>{formatToRupiah(buyPrice)}</span
-						>
+					<div
+						class="flex w-[240px] shrink-0 items-center px-4 py-1 shadow-vertical"
+					>
+						<span>{formatToRupiah(buyPrice)}</span>
 					</div>
 
-					<div class="flex w-full items-center justify-between px-4 py-2">
-						<span class="w-full text-base leading-none"
-							>{formatToRupiah(sellPrice)}</span
-						>
+					<div
+						class="flex w-[240px] shrink-0 items-center px-4 py-1 shadow-vertical"
+					>
+						<span>{formatToRupiah(sellPrice)}</span>
 					</div>
 
-					<div class="flex w-full items-center justify-between px-4 py-2">
-						<span class="w-full text-base leading-none"
-							>{formatToRupiah(quantity * buyPrice)}</span
-						>
+					<div
+						class="flex w-[200px] shrink-0 items-center px-4 py-1 shadow-vertical"
+					>
+						<span>{formatToRupiah(quantity * buyPrice)}</span>
 					</div>
 
-					<div class="flex w-full items-center justify-between px-4 py-2">
-						<span class="w-full text-base leading-none"
-							>{formatToRupiah(quantity * sellPrice)}</span
-						>
+					<div
+						class="flex w-[200px] shrink-0 items-center px-4 py-1 shadow-vertical"
+					>
+						<span>{formatToRupiah(quantity * sellPrice)}</span>
 					</div>
 
-					<div class="flex w-full items-center justify-center gap-2 px-4 py-2">
-						<div class="rounded-md bg-yellow-500 p-1">
-							<Icon props={{ name: "edit", width: "20px", height: "20px" }} />
+					<div
+						class="flex w-[80px] shrink-0 items-center justify-center gap-1 px-4"
+					>
+						<div class="rounded bg-accent-200 p-1">
+							<Icon
+								props={{
+									name: "edit",
+									classes: clsx("size-4 text-yellow-500"),
+								}}
+							/>
 						</div>
 
-						<div class="rounded-md bg-red-500 p-1">
-							<Icon props={{ name: "delete", width: "20px", height: "20px" }} />
+						<div class="rounded bg-accent-200 p-1">
+							<Icon
+								props={{ name: "delete", classes: clsx("size-4 text-red-500") }}
+							/>
 						</div>
 					</div>
 				</div>
 			{/each}
+		</div>
 
-			<div
-				class="flex w-full items-center justify-between bg-havelock-blue-50 px-4 py-2 text-sm leading-none"
-			>
-				<div class="flex items-center gap-8">
-					<div class="flex items-center gap-2">
-						<span
-							class="rounded-md border border-havelock-blue-100 bg-white p-2 font-medium"
-							>15</span
-						>
-
-						<span class="text-black/50">Produk per halaman</span>
-					</div>
-
-					<span class="font-medium">1 - 15 dari 200 produk</span>
+		<footer
+			class="flex w-full items-center justify-between bg-accent-100 px-4 py-2 text-sm leading-none"
+		>
+			<div class="flex items-center gap-2">
+				<div
+					class="flex items-center justify-center rounded border border-accent-950/10 px-2 py-1"
+				>
+					<span class="font-medium">15</span>
 				</div>
 
-				<div class="flex items-center gap-2">
-					<span
-						class="rounded-md border border-havelock-blue-100 bg-white p-2 font-medium"
-						>1</span
-					>
+				<span>Produk per halaman</span>
 
-					<span class="font-medium">dari 14 halaman</span>
+				<span class="font-medium text-accent-950">1 - 15 dari 200 produk</span>
+			</div>
 
-					<div class="flex items-center gap-1">
-						<div class="rounded-md bg-white p-1">
-							<Icon props={{ name: "chevronLeft", fill: "#000000" }} />
-						</div>
+			<div class="flex items-center gap-2">
+				<div
+					class="flex items-center justify-center rounded border border-accent-950/10 px-2 py-1"
+				>
+					<span class="font-medium">1</span>
+				</div>
 
-						<div class="rounded-md bg-white p-1">
-							<Icon props={{ name: "chevronRight", fill: "#000000" }} />
-						</div>
-					</div>
+				<span class="font-medium">dari 14 halaman</span>
+
+				<div
+					class="flex size-6 items-center justify-center rounded border border-accent-950/10"
+				>
+					<Icon props={{ name: "chevronLeft", classes: "size-5" }} />
+				</div>
+
+				<div
+					class="flex size-6 items-center justify-center rounded border border-accent-950/10"
+				>
+					<Icon props={{ name: "chevronRight", classes: "size-5" }} />
 				</div>
 			</div>
-		</div>
+		</footer>
 	</section>
 </main>
